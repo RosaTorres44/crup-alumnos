7d683d7bfa289abd9b116e23ddb282ae9e9b5308
Rosita Torres <rosamtorres4492@gmail.com>
2024-12-12 21:57:00 -0500

    Subiendo proyecto CRUD Alumnos

---
+++ entorno.sh
@@ -0,0 +1,11 @@
+#para crear el ambiente 
+python -m venv venv
+#activar el env 
+source venv/bin/activate 
+
+#desactivar el env 
+deactivate 
+
+#se crear el archivo de requerimientos 
+pip install -r requeriments.txt 
+
